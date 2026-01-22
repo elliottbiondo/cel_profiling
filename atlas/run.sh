102eae910384242ccf64836ac201c33fc51b9d49
Elliott Biondo <veb@ornl.gov>
2026-01-22 17:50:20 -0500

    More

--- atlas/run.sh
+++ atlas/run.sh
@@ -1,3 +1,3 @@
-rm -rf stdout.json stderr exception.json 
+rm -rf stdout.json stderr exception.json
 CELER_LOG=debug CELER_LOG_LOCAL=debug celer-sim run.json 1> stdout.json 2> stderr
 jq '.["result"]["exception"]' stdout.json > exception.json
