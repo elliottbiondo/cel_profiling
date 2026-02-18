a966f48447d1623b5e3f351a5dd943dbf65881c3
Elliott Biondo <veb@ornl.gov>
2026-02-18 16:19:25 -0500

    Improving make_bih_dot script

--- scripts/make_bih_dot.py
+++ scripts/make_bih_dot.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-
 import argparse
 import json
 from pathlib import Path
-
 
 def make_dot(universe_id, tree):
     lines = []
@@ -16,7 +13,7 @@ def make_dot(universe_id, tree):
             axis = node[1]
             left_plane = float(node[3][0])
             right_plane = float(node[3][1])
-            label = "{}=({:.4f}, {:.4f})".format(axis, left_plane, right_plane)
+            label = "{}=({:.2f}, {:.2f})".format(axis, left_plane, right_plane)
             lines.append('  n{} [label="{}"];'.format(i, label))
         else:
             vols = ", ".join("{}".format(v) for v in node[1])
@@ -24,20 +21,31 @@ def make_dot(universe_id, tree):
 
     for i, node in enumerate(tree):
         if node[0] == "i":
-            left_child = int(node[2][0])
-            right_child = int(node[2][1])
+            left_child = node[2][0]
+            right_child = node[2][1]
             lines.append('  n{} -> n{} [label="L"];'.format(i, left_child))
             lines.append('  n{} -> n{} [label="R"];'.format(i, right_child))
 
     lines.append("}")
     return "\n".join(lines)
 
+def print_diagnostic(bih_data):
+
+    depth = bih_data["depth"]
+    nvol = bih_data["num_finite_bboxes"]
+    ninf = bih["num_infinite_bboxes"]
+    template = "{:>8} {:>8} {:>8} {:>8}"
+
+    out = template.format("uid", "depth", "num_vols", "num_inf")
+    for i in range(len(structure)):
+        out += template.format(i, depth[i], nvol[i], ninf[i])
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Print BIH summary and write bih_<universe>.dot files."
     )
-    parser.add_argument("json_file", type=Path, help="Path to Celeritas output JSON")
+    parser.add_argument("json_file", type=Path, help="Path to output JSON")
     parser.add_argument(
         "universe",
         nargs="*",
@@ -45,34 +53,21 @@ def main():
         help="Optional universe index(es). If omitted, write all.",
     )
     args = parser.parse_args()
+    
+    # Read all necessary data from json
+    with args.json_file.open("r") as f:
+        bih_data = json.load(f)["internal"]["orange"]["bih_metadata"]
 
-    with args.json_file.open("r", encoding="utf-8") as f:
-        data = json.load(f)
-
-    bih = data["internal"]["orange"]["bih_metadata"]
-    depth = bih["depth"]
-    num_vols = bih["num_finite_bboxes"]
-    num_inf_vols = bih["num_infinite_bboxes"]
-    structure = bih["structure"]
-
-    print("{:>8} {:>6} {:>8} {:>9}".format("univ", "depth", "num_vols", "num_inf"))
-    for i in range(len(structure)):
-        print(
-            "{:>8} {:>6} {:>8} {:>9}".format(
-                i, int(depth[i]), int(num_vols[i]), int(num_inf_vols[i])
-            )
-        )
-
+    # Figure out what universes to write dot file for
     if len(args.universe) > 0:
-        selected = args.universe
+        uids = args.universe
     else:
-        selected = list(range(len(structure)))
+        uids = list(range(len(bih_data["structure"])))
 
-    for uid in selected:
-        tree = structure[uid]["tree"]
-        dot = make_dot(uid, tree)
-        out_path = Path.cwd() / "bih_{}.dot".format(uid)
-        out_path.write_text(dot, encoding="utf-8")
+    # Write dot files for the selected universes
+    for uid in uids:
+        with open("bih_{}.dot".format(uid), "w") as f:
+            f.write(make_dot(uid, bih_data["structure"][uid]["tree"]))
 
 
 if __name__ == "__main__":
