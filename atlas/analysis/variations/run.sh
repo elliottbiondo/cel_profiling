ce8446da73715f581ddd6eab5924f6cfc396a5fb
Elliott Biondo <veb@ornl.gov>
2026-04-21 14:27:26 -0400

    Run scripts

--- atlas/analysis/variations/run.sh
+++ atlas/analysis/variations/run.sh
@@ -1,11 +1,12 @@
-CELER_BIH_MAX_LEAF_SIZE=316 /scratch/veb/build/celeritas-release-orange/bin/celer-sim inp-orange.json 1> orange.json
+ORANGE_BIH_MAX_LEAF_SIZE=1 /scratch/veb/build/celeritas-release-orange/bin/celer-sim inp-orange.json 1> out-orange.json
 echo "ORANGE"
-jq ".result.runner.time.total" orange.json
-jq '.result.runner.time.actions.["along-step-neutral"]' orange.json
-jq '.result.runner.time.actions.["along-step-uniform-msc"]' orange.json
+jq ".result.runner.time.total" out-orange.json
+jq '.result.runner.time.actions.["along-step-uniform-msc"]' out-orange.json
+jq '.result.runner.time.actions.["along-step-neutral"]' out-orange.json
 
+
+CUDA_HEAP_SIZE=10000000 CUDA_STACK_SIZE=32000 /scratch/veb/build/celeritas-release/bin/celer-sim inp-vg1.json 1> out-vg1.json
 echo "VG1"
-/scratch/veb/build/celeritas-release/bin/celer-sim inp-vg1.json 1> vg1.json
-jq ".result.runner.time.total" vg1.json
-jq '.result.runner.time.actions.["along-step-neutral"]' vg1.json
-jq '.result.runner.time.actions.["along-step-uniform-msc"]' vg1.json
+jq ".result.runner.time.total" out-vg1.json
+jq '.result.runner.time.actions.["along-step-uniform-msc"]' out-vg1.json
+jq '.result.runner.time.actions.["along-step-neutral"]' out-vg1.json
---
+++ dune/analysis/variations/run.sh
@@ -0,0 +1,9 @@
+ORANGE_BIH_MAX_LEAF_SIZE=1 /scratch/veb/build/celeritas-release-orange/bin/celer-optical inp-orange.json 1> out-orange.json
+echo "ORANGE"
+jq ".result.time.total" out-orange.json
+jq '.result.time.actions.["along-step"]' out-orange.json
+
+CUDA_HEAP_SIZE=10000000 CUDA_STACK_SIZE=32000 /scratch/veb/build/celeritas-release/bin/celer-optical inp-vg1.json 1> out-vg1.json
+echo "VG1"
+jq ".result.time.total" out-vg1.json
+jq '.result.time.actions.["along-step"]' out-vg1.json
