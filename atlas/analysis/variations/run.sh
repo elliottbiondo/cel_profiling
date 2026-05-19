d5f6a95a0b597cd78d5043741b9e65b5ae282746
Elliott Biondo <veb@ornl.gov>
2026-05-19 13:19:05 -0400

    Update run scripts

--- atlas/analysis/variations/run.sh
+++ atlas/analysis/variations/run.sh
@@ -1,11 +1,11 @@
-ORANGE_BIH_MAX_LEAF_SIZE=1 /scratch/veb/build/celeritas-release-orange/bin/celer-sim inp-orange.json 1> out-orange.json
+CUDA_VISIBLE_DEVICES=1 ORANGE_BIH_MAX_LEAF_SIZE=1 /scratch/veb/build/celeritas-release-orange/bin/celer-sim inp-orange.json 1> out-orange.json
 echo "ORANGE"
 jq ".result.runner.time.total" out-orange.json
 jq '.result.runner.time.actions.["along-step-uniform-msc"]' out-orange.json
 jq '.result.runner.time.actions.["along-step-neutral"]' out-orange.json
 
 
-CUDA_HEAP_SIZE=10000000 CUDA_STACK_SIZE=32000 /scratch/veb/build/celeritas-release/bin/celer-sim inp-vg1.json 1> out-vg1.json
+CUDA_VISIBLE_DEVICES=1 CUDA_HEAP_SIZE=10000000 CUDA_STACK_SIZE=32000 /scratch/veb/build/celeritas-release/bin/celer-sim inp-vg1.json 1> out-vg1.json
 echo "VG1"
 jq ".result.runner.time.total" out-vg1.json
 jq '.result.runner.time.actions.["along-step-uniform-msc"]' out-vg1.json
