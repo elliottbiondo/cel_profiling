2d33b62469a6e7af99f7e0872898130b09268198
Elliott Biondo <veb@ornl.gov>
2026-09-11 17:58:09 -0400

    Update notional dune results

--- notional_dune/run_all.py
+++ notional_dune/run_all.py
@@ -3,14 +3,11 @@ import os
 
 template = open("inp_template.json").read()
 
-cases = list(range(11)) + [25, 50, 100]
-
-for g in cases: 
+for g in range(10): 
     open("inp_temp.json", "w").write(template.replace("GEONUM", str(g)))
-    os.system(f"CUDA_VISIBLE_DEVICES=1 /scratch/veb/build/celeritas-release-orange/bin/celer-optical inp_temp.json 1> out_{g}.json")
+    os.system(f"CUDA_VISIBLE_DEVICES=0 /scratch/veb/build/celeritas-release-orange/bin/celer-optical inp_temp.json 1> out_{g}.json")
     os.remove("inp_temp.json")
 
-for g in cases:
-    printf("g:\n\t")
+for g in range(10):
     os.system(f'jq ".result.time.total" out_{g}.json')
 
